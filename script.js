56cfe045b8b0d1e6ca0597150cf51bdc143d3576
lvingonzales <lvingonzales15@gmail.com>
2024-11-22 13:24:30 -0500

    Start and finish RPS Ui update

--- script.js
+++ script.js
@@ -2,55 +2,100 @@
 // generate Computer input
 // compare the two inputs and decide the winner
 
-let humanChoice;
+let winnerDecided = false;
+let roundNumber = 0;
 let computerChoice;
 let humanScore = 0;
 let computerScore = 0;
 
-function PlayGame (){
-    for (i = 1; i <= 5; i++){
-        PlayRound()
-    }
+const div = document.querySelector("#results")
+    div.style.border = "3px solid black";
+    div.style.borderRadius = "5px";
+    div.style.backgroundColor = "grey";
+    div.style.width = "300px";
+    div.style.height = "400px";
 
-    if (humanScore > computerScore){
-        console.log ("SCORE IS: CPU: " + computerScore + "-" + humanScore + " :YOU")
-        console.log ("YOU WIN!")
-    } else if (computerScore > humanScore){
-        console.log ("SCORE IS: CPU: " + computerScore + "-" + humanScore + " :YOU")
-        console.log ("CPU WINS!")
-    } else {
-        console.log ("SCORE IS: CPU: " + computerScore + "-" + humanScore + " :YOU")
-        console.log ("ITS A TIE!")
-    }
-}
 
-function PlayRound (){
+const buttons = document.querySelectorAll("button")
+
+const resultsHeader = document.createElement ("h3");
+resultsHeader.setAttribute("class", "text");
+resultsHeader.textContent = "Results: ";
+div.appendChild(resultsHeader);
+
+const displayRounds = document.createElement ("p");
+displayRounds.setAttribute("class", "text roundCard");
+displayRounds.textContent = "ROUND: " + roundNumber;
+div.appendChild(displayRounds);
+
+const humanChoiceDisplay = document.createElement("p");
+humanChoiceDisplay.setAttribute("class", "text");
+humanChoiceDisplay.textContent = "YOU: "
+div.appendChild(humanChoiceDisplay);
+
+const cpuChoiceDisplay = document.createElement("p");
+cpuChoiceDisplay.setAttribute("class", "text");
+cpuChoiceDisplay.textContent = "CPU: "
+div.appendChild(cpuChoiceDisplay);
+
+const scorecardHeader = document.createElement("h3");
+scorecardHeader.setAttribute("class", "text");
+scorecardHeader.textContent = "SCORES: ";
+div.appendChild(scorecardHeader);
+
+const scores = document.createElement("p");
+scores.setAttribute("class", "text");
+scores.style.marginBottom = "5px";
+scores.style.textAlign = "center";
+scores.textContent = "CPU: " + computerScore + " - " + humanScore + " :YOU";
+div.appendChild(scores);
+
+buttons.forEach((button)  => {
+    button.addEventListener("click", () => {
+        
+        let choice = button.textContent;
+
+        if (winnerDecided === true){
+            roundNumber = 0;
+            humanScore = 0;
+            computerScore = 0;
+            resultsHeader.textContent = "Results: ";
+            winnerDecided = false;
+        }
+
+        PlayRound(choice);
+        humanChoiceDisplay.textContent = "YOU: " + choice;
+        cpuChoiceDisplay.textContent = "CPU: " + computerChoice;
+        displayRounds.textContent = "ROUND: " + roundNumber;
+
+    })
+})
+
+function PlayRound (humanChoice){
     ComputerLogic();
-    HumanPrompt();
 
-    if (computerChoice === humanChoice.toLowerCase()){
-        console.log ("CPU: " + computerChoice + " VS " + "YOU: " + humanChoice);
-        console.log("TIE");
-    }   else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors"){
-        console.log ("CPU: " + computerChoice + " VS " + "YOU: " + humanChoice);
-        console.log("Player Wins");
+    if (computerChoice === humanChoice){
+    }   else if (humanChoice === "Rock" && computerChoice === "Scissors"){
         humanScore++;
-    }   else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock"){
-        console.log ("CPU: " + computerChoice + " VS " + "YOU: " + humanChoice);
-        console.log("Player Wins");
+    }   else if (humanChoice === "Paper" && computerChoice === "Rock"){
         humanScore++;
-    }   else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper"){
-        console.log ("CPU: " + computerChoice + " VS " + "YOU: " + humanChoice);
-        console.log("Player Wins");
+    }   else if (humanChoice === "Scissors" && computerChoice === "Paper"){
         humanScore++;
-    } else if (humanChoice.toLowerCase() !== "rock" &&
-                humanChoice.toLowerCase() !== "paper" &&
-                humanChoice.toLowerCase() !== "scissors"){
-        console.log ("INVALID INPUT")
-    }else {
-        console.log ("CPU: " + computerChoice + " VS " + "YOU: " + humanChoice);
-        console.log ("Computer Wins");
+    } else {
         computerScore++;
+    }
+
+    scores.textContent = "CPU: " + computerScore + " - " + humanScore + " :YOU";
+
+    roundNumber++;
+
+    if (computerScore === 5 || humanScore === 5){
+        winnerDecided = true;
+        if (computerScore === 5){
+            resultsHeader.textContent = "Results: CPU WINS!";
+        } else {
+            resultsHeader.textContent = "Results: YOU WIN";
+        }
     }
 }
 
@@ -59,23 +104,15 @@ function ComputerLogic(){
 
     switch (choice){
         case 0:
-            computerChoice = "rock";
+            computerChoice = "Rock";
             break;
         case 1:
-            computerChoice = "paper";
+            computerChoice = "Paper";
             break;
         case 2:
-            computerChoice = "scissors";
+            computerChoice = "Scissors";
             break;
     }
 
     return null;
 }
-
-function HumanPrompt(){
-    humanChoice = prompt("Enter your choice: ");
-
-    return null;
-}
-
-PlayGame();
